c693c4cef4ee7a5f6882b0d63e73efcc560cf5e0
ChaithanyaKumarreddy <107611510+ChaithanyaKumarreddy@users.noreply.github.com>
2025-07-27 10:22:41 +0530

    Add files via upload

---
+++ TaskAssignment/Task3/Task_3_1.java
@@ -0,0 +1,13 @@
+package Task3;
+
+public class Task_3_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final double PI =3.14;
+		System.out.println("PI is"+PI);
+		
+//final variable value can not be changed Once the value Initialized
+	}
+
+}
